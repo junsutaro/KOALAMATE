f41beba7d6affb59a30c1c7e56533b744de22f63
Hyeonwoo_Cho <chw03177@gmail.com>
2024-01-19 17:49:16 +0900

    Add: something

--- front_end/koala/src/App.js
+++ front_end/koala/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('/hello')
+    .then(response => {
+      setMessage(response.data);
+    })
+    .catch(error => {
+      console.error('There was an error!', error);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <h1>
-        asdfasdf
-      </h1>
+      <header className="App-header">
+        <p>Server says: {message}</p>
+      </header>
     </div>
   );
 }
