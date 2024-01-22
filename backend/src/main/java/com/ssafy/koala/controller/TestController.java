da969d757953205c9da17dc042f29d55b5c7331f
장민성 <hs0765@naver.com>
2024-01-22 15:25:27 +0900

    basic architecture

--- backend/src/main/java/com/ssafy/koala/controller/TestController.java
+++ backend/src/main/java/com/ssafy/koala/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.ssafy.koala;
+package com.ssafy.koala.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
