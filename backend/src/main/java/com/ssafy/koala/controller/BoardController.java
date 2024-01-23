9e9fee50325b05652d3cf0e6f8471f28b3741678
장민성 <hs0765@naver.com>
2024-01-23 16:35:59 +0900

    FEAT:board CRUD

--- backend/src/main/java/com/ssafy/koala/controller/BoardController.java
+++ backend/src/main/java/com/ssafy/koala/controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.ssafy.koala.controller;
 
+import com.ssafy.koala.dto.BoardDto;
+import com.ssafy.koala.model.BoardModel;
 import com.ssafy.koala.service.BoardService;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -22,5 +25,55 @@ public class BoardController {
 		ResponseEntity response = null;
 		response = new ResponseEntity<>(boardService.getPageEntities(pageAble),HttpStatus.OK);
 		return response;
+	}
+
+	@GetMapping("/view")
+	public Object viewBoard(@RequestParam Long id) {
+		ResponseEntity response = null;
+
+		response = new ResponseEntity<>(boardService.getBoardById(id),HttpStatus.OK);
+		return response;
+	}
+
+	@PostMapping("/write")
+	public Object writeBoard(@RequestBody BoardDto board) {
+		ResponseEntity response = null;
+
+//		BoardModel boardModel = new BoardModel();
+//		boardModel.setId(board.getId());
+//		boardModel.setTitle(board.getTitle());
+//		boardModel.setContent(board.getContent());
+//		boardModel.setDate(board.getDate());
+//		boardModel.setViews(board.getViews());
+//		boardModel.setNickname(board.getNickname());
+
+		BoardDto boardDto = boardService.createBoard(board);
+		response = new ResponseEntity<>(boardDto,HttpStatus.OK);
+
+		return response;
+	}
+
+	@PutMapping("modify")
+	public Object modifyBoard(@RequestParam Long id, @RequestBody BoardDto board) {
+		ResponseEntity response = null;
+
+		BoardDto boardDto = boardService.updateBoard(id, board);
+		response = new ResponseEntity<>(boardDto,HttpStatus.OK);
+
+		return response;
+	}
+
+	@DeleteMapping("delete")
+	public Object deleteBoard(@RequestParam Long id) {;
+		try {
+			boardService.deleteBoard(id);
+			return new ResponseEntity<>("Board with ID " + id + " deleted successfully.", HttpStatus.OK);
+		} catch (EmptyResultDataAccessException e) {
+			// 해당 ID에 해당하는 엔티티가 존재하지 않는 경우
+			return new ResponseEntity<>("Board with ID " + id + " not found.", HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			// 기타 예외 처리
+			return new ResponseEntity<>("Error deleting board with ID " + id, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
--- backend/src/main/java/com/ssafy/koala/dto/BoardDto.java
+++ backend/src/main/java/com/ssafy/koala/dto/BoardDto.java
@@ -2,9 +2,15 @@ package com.ssafy.koala.dto;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class BoardDto {
 	private long id;
 	private String title;
+	private String content;
+	private Date date;
+	private int views;
+	private String nickname;
 
 }
