08ef4e8a8d1a3b24d81ef419ea02c5adf3b4c9e7
이준수 <s2s2s2x101@gmail.com>
2024-01-24 17:52:03 +0900

    FEAT: refridgerator frame

---
+++ backend/src/main/java/com/ssafy/koala/repository/RefrigeratorRepository.java
@@ -0,0 +1,13 @@
+package com.ssafy.koala.repository;
+
+import com.ssafy.koala.model.RefrigeratorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefrigeratorRepository extends JpaRepository<RefrigeratorModel, Long> {
+
+        Optional<RefrigeratorModel> findByUserId(Long userId);
+}
