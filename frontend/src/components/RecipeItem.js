83843569c3403c6b0cfe2b9e28fca60378d1f3ea
SUSIE <pyunsusie@naver.com>
2024-01-25 09:30:56 +0900

    feat: recipeitem

--- frontend/src/components/RecipeItem.js
+++ frontend/src/components/RecipeItem.js
@@ -1,5 +1,28 @@
 import style from "components/RecipeItem.module.css";
 
+// 더미 데이터
+const dummy = [
+	{
+		id: 1,
+		recipeName: '레시피1',
+		writerName: '작성자1',
+		recipeImg: '이미지 URL1',
+		percent: '도수1',
+		base: '베이스주1',
+		tag: '태그1',
+	},
+	{
+		id: 2,
+		recipeName: '레시피2',
+		writerName: '작성자2',
+		recipeImg: '이미지 URL2',
+		percent: '도수2',
+		base: '베이스주2',
+		tag: '태그2',
+	},
+	// 나머지 레시피 데이터...
+];
+
 export default function RecipeItem({
 		id,
 		recipeName,
