037020d086f6967f49083d09798e50d7c1a3a697
장민성 <hs0765@naver.com>
2024-02-04 21:49:07 +0900

    FIX:voice chat controller
    Former-commit-id: 626f7cb7c52db3b39ccb8949ee789ae487c9e88b

--- backendChat/src/main/java/com/ssafy/koala/controller/chat/VoiceChatController.java
+++ backendChat/src/main/java/com/ssafy/koala/controller/chat/VoiceChatController.java
@@ -8,14 +8,14 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@RequestMapping("/voice")
+@Component
+@CrossOrigin(origins = "http://localhost:3000")
 public class VoiceChatController {
 
     @Value("${OPENVIDU_URL}")
@@ -35,9 +35,10 @@ public class VoiceChatController {
      * @param params The Session properties
      * @return The Session ID
      */
-    @PostMapping("/voice/sessions")
+    @PostMapping("/sessions")
     public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
             throws OpenViduJavaClientException, OpenViduHttpException {
+        System.out.println("in session");
         SessionProperties properties = SessionProperties.fromJson(params).build();
         Session session = openvidu.createSession(properties);
         return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
@@ -48,10 +49,11 @@ public class VoiceChatController {
      * @param params    The Connection properties
      * @return The Token associated to the Connection
      */
-    @PostMapping("/voice/sessions/{sessionId}/connections")
+    @PostMapping("/sessions/{sessionId}/connections")
     public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
                                                    @RequestBody(required = false) Map<String, Object> params)
             throws OpenViduJavaClientException, OpenViduHttpException {
+        System.out.println("in conntection");
         Session session = openvidu.getActiveSession(sessionId);
         if (session == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
