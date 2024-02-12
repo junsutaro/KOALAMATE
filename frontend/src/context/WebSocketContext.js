7d2a0862b207906b2b6454037c9082b738045b90
장민성 <hs0765@naver.com>
2024-02-13 01:04:14 +0900

    fix:chat test2...
    Former-commit-id: 39c4e69a44d7991454e426ee9108674a495a6d75

--- frontend/src/context/WebSocketContext.js
+++ frontend/src/context/WebSocketContext.js
@@ -66,7 +66,7 @@ export const WebSocketProvider = ({children}) => {
             setIsRefresh(false);
         }
         console.log(roomStatus);
-    }, [roomStatus, stompClient]);
+    }, [roomStatus]);
 
 
 
