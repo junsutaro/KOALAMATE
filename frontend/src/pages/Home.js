24a80f56a4cf27a72ce9f6a776c99fa8e31042a6
조현우 <chw03177@gmail.com>
2024-02-15 12:37:01 +0900

    FEAT: 디자인 대대적 수정
    Former-commit-id: e7ca14d8987aa9e1dc63230d180f9783f4d8deb9

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,17 +7,34 @@ import {
 	Paper, TextField,
 	Typography,
 } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 import ImageSlider from 'components/home/ImageSlider';
 import HomeRecipeList from '../components/home/HomeRecipeList';
 import SimpleMap from 'components/home/SimpleMap';
 import Stack from '@mui/material/Stack';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
 	const [open, setOpen] = useState(false);
 	const handleOpen = () => setOpen(true);
 	const handleClose = () => setOpen(false);
+	const [email, setEmail] = useState('');
+
+	const { isLoggedIn } = useSelector((state) => state.auth);
+
+	const navigate = useNavigate();
+
+	// 이메일 입력값 변경 핸들러
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
+
+	// "Start now" 버튼 클릭 핸들러
+	const handleStartNow = () => {
+		// 이메일 값과 함께 회원가입 페이지로 이동
+		navigate('/signup', { state: { email } });
+	};
 
 	const style = {
 		position: 'absolute',
@@ -41,84 +58,100 @@ const Home = () => {
 				flexDirection: 'column',
 				alignItems: 'center',
 			}}>
-				<Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
-					<Typography
-						component="h1"
-						variant="h1"
-						sx={{
-							display: 'flex',
-							flexDirection: { xs: 'column', md: 'row' },
-							alignSelf: 'center',
-							textAlign: 'center',
-						}}
-					>
-						코알라&nbsp;
+				{!isLoggedIn &&
+					<Stack spacing={2} useFlexGap
+					       sx={{ width: { xs: '100%', sm: '70%' } }} mb={10}>
 						<Typography
-							component="span"
+							component="h1"
 							variant="h1"
 							sx={{
-								color: (theme) =>
-									theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+								display: 'flex',
+								flexDirection: { xs: 'column', md: 'row' },
+								alignSelf: 'center',
+								textAlign: 'center',
 							}}
 						>
-							메이트
+							코알라&nbsp;
+							<Typography
+								component="span"
+								variant="h1"
+								sx={{
+									color: (theme) =>
+										theme.palette.mode === 'light'
+											? 'primary.main'
+											: 'primary.light',
+								}}
+							>
+								메이트
+							</Typography>
 						</Typography>
-					</Typography>
-					<Typography variant="body1" textAlign="center" color="text.secondary">
-						Explore our cutting-edge dashboard, delivering high-quality solutions
-						tailored to your needs. <br />
-						Elevate your experience with top-tier features and services.
-					</Typography>
-					<Stack
-						direction={{ xs: 'column', sm: 'row' }}
-						alignSelf="center"
-						spacing={1}
-						useFlexGap
-						sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
-					>
-						<TextField
-							id="outlined-basic"
-							hiddenLabel
-							size="small"
-							variant="outlined"
-							aria-label="Enter your email address"
-							placeholder="Your email address"
-							inputProps={{
-								autocomplete: 'off',
-								ariaLabel: 'Enter your email address',
-							}}
-						/>
-						<Button variant="contained" color="primary">
-							Start now
-						</Button>
+						<Typography variant="body1" textAlign="center"
+						            color="text.secondary">
+							주변의 술 친구를 찾고 공유하고 소통해보세요. <br/>
+							냉장고 꾸미기로 개성을 표현하고 취향에 맞는 친구를 찾아보세요.
+						</Typography>
+						<Stack
+							direction={{ xs: 'column', sm: 'row' }}
+							alignSelf="center"
+							spacing={1}
+							useFlexGap
+							sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+						>
+							<form onSubmit={(event => {
+								event.preventDefault();
+								handleStartNow();
+							})}
+							      style={{ display: 'flex', gap: '16px' }}
+							>
+							<TextField
+								id="outlined-basic"
+								hiddenLabel
+								size="small"
+								variant="outlined"
+								aria-label="Enter your email address"
+								placeholder="Your email address"
+								value={email}
+								onChange={handleEmailChange}
+								inputProps={{
+									autocomplete: 'off',
+									ariaLabel: 'Enter your email address',
+								}}
+							/>
+							<Button type="susbmit" variant="contained" color="primary">
+								지금 시작
+							</Button>
+							</form>
+						</Stack>
+						<Typography variant="caption" textAlign="center"
+						            sx={{ opacity: 0.8 }}>
+							&quot;지금 시작&quot; 버튼을 클릭하면 &nbsp;
+							<Link href="#" color="primary">
+								Terms & Conditions
+							</Link>
+							에 동의하는 것으로 간주합니다.
+						</Typography>
 					</Stack>
-					<Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
-						By clicking &quot;Start now&quot; you agree to our&nbsp;
-						<Link href="#" color="primary">
-							Terms & Conditions
-						</Link>
-						.
-					</Typography>
-				</Stack>
+				}
 				{/*<Box mb={4}>*/}
 				{/*	<Paper elevation={3} sx={{background: "white"}}>*/}
 				{/*		<ImageSlider/>*/}
 				{/*	</Paper>*/}
 				{/*</Box>*/}
-				<Box mb={4}>
-						<Typography variant={'h2'} align={'center'}>
-							궁금한 레시피
-						</Typography>
-						<Typography variant={'h5'} align={'center'}>
-							너무나 복잡한 칵테일 레시피, 쉽게 찾고 공유해요
-						</Typography>
-						<HomeRecipeList optionNum={1} currentPage={5}/>
+				<Box mb={10} sx={{ width: '100%' }}> {/* 화면 전체 너비를 가지도록 설정 */}
+					<ImageSlider/>
+				</Box>
+				<Box mb={10}>
+					<Typography variant={'h2'} align={'center'}>
+						궁금한 레시피
+					</Typography>
+					<Typography variant={'h5'} align={'center'}>
+						너무나 복잡한 칵테일 레시피, 쉽게 찾고 공유해요
+					</Typography>
+					<HomeRecipeList optionNum={1} currentPage={5}/>
 				</Box>
 				<Box mb={4}>
-					<Paper elevation={3} sx={{background: "white"}}>
-						<h2>내 주변의 냉장고 & 메이트 찾기</h2>
-						<SimpleMap/>
-					</Paper>
+					<h2>내 주변의 냉장고 & 메이트 찾기</h2>
+					<SimpleMap/>
 				</Box>
 			</Container>
 		</div>
